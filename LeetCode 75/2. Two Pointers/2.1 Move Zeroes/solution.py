6a9bc853abe34ad4f4da6b230c9a6930772797a4
vamshidhar_kandi <124335309+kandiv12@users.noreply.github.com>
2025-03-23 23:52:45 -0400

    Add files via upload

---
+++ LeetCode 75/2. Two Pointers/2.1 Move Zeroes/solution.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        size = len(nums)
+        ptr = 0
+
+        for i in range(size):
+            if nums[i] != 0:
+                nums[i], nums[ptr] = nums[ptr], nums[i]
+                ptr += 1
+        
+        for i in range(ptr, size):
+            nums[i] = 0
+
+# driver code
+
+tests = [
+    [0, 1, 0, 3, 12],
+    [0]
+]
+
+solution = Solution()
+
+for test in tests:
+    solution.moveZeroes(test)
+    print(test)
